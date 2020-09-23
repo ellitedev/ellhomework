01197e7d89a3c0a6c73e3c971dc8b9fd3c05cb61
Ellite <46927093+nekoellie@users.noreply.github.com>
2020-09-23 10:21:29 +0200

    update

--- HTML/Python/something.py
+++ HTML/Python/something.py
@@ -1,2 +1,9 @@
 print("Hello World")
 print("hello world")
+
+first_name = "Ellie"
+Last_name = "Leijten"
+print(first_name + " " + Last_name)
+text = "leefttijd: %s"
+getal = 19
+print(text % getal)
