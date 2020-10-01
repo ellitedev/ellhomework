4ef5afe345cedd408752b12c33aa18722f5dbfea
nekoellie <46927093+nekoellie@users.noreply.github.com>
2020-10-01 14:07:40 +0200

    update

--- Python/something.py
+++ Python/something.py
@@ -1,2 +1,14 @@
-print("Hello World")
-print("hello world")
+getal1 = input("Geef getal 1 op: ") 
+getal2 = input("Geef getal 2 op: ") 
+
+getal3 = int(input("Geef getal 3 op: ")) 
+getal4 = int(input("Geef getal 4 op: ")) 
+
+antwoord12 = getal1 + getal2
+antwoord34 = getal3 + getal4
+
+zin1 = "Het antwoord van getal1 + getal2 is %s"
+zin2 = "Het antwoord van getal3 + getal4 is %s"
+
+print(zin1 % antwoord12)
+print(zin2 % antwoord34)
